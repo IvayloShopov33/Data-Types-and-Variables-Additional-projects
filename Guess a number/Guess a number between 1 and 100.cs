87695a10e4d168c1773196e75c8975f002516a9b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-05-22 23:16:10 +0300

    Added a new project
    The project name is "Guess a number between 1 and 100"

---
+++ Guess a number/Guess a number between 1 and 100.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Guess_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+            int attempts = 10;
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+                attempts--;
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber > computerNumber)
+                        Console.WriteLine("Too High");
+                    else
+                        Console.WriteLine("Too Low");
+                }
+                else
+                    Console.WriteLine("Invalid input");
+                if (attempts > 0)
+                {
+                    Console.WriteLine($"You have {attempts} left!");
+                }
+                else
+                {
+                    Console.WriteLine("You do not have any attempts left!");
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+            }
+        }
+    }
+}
